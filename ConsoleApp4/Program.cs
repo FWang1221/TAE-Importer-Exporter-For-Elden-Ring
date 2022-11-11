78597b5901c35fc1b5254552e6e3814210d67c33
FWang1221 <112018360+FWang1221@users.noreply.github.com>
2022-11-11 08:43:35 -0500

    Update Program.cs
    added seeds for batch edits

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp4 //Sorry to anyone who wants to read code written by a fuck
 {
     class Program
     {
+        static class Globals
+        {
+            public static bool isSeed = false;
+        }
         static void extractParamsMany(string directoryHere, string oldDir, List<string> paramTypes, int eventTypeNum)
         {
             
@@ -92,6 +96,7 @@ namespace ConsoleApp4 //Sorry to anyone who wants to read code written by a fuck
                     }
                 }
             }
+            eventReader.Add(directoryHere);
             File.WriteAllLines(oldDir + "/" + directoryHere + "EventsFile.csv", eventReader.Select(x => x.ToString()));
             //File.WriteAllLines(oldDir + "/" + directoryHere + "EventGroupsFile.csv", eventGroupReader.Select(x => x.ToString()));
         }
@@ -145,6 +150,11 @@ namespace ConsoleApp4 //Sorry to anyone who wants to read code written by a fuck
             }
             foreach (string line in lines)
             {
+                if (line[0] == 'c')
+                {
+                    newPath = line.Replace(",", "");
+                    break;
+                }
                 foreach (var taeFile in taeFiles.Where(x => x.Bytes.Length > 0))
                 {
                     TAE tae = TAE.Read(taeFile.Bytes);
@@ -198,13 +208,129 @@ namespace ConsoleApp4 //Sorry to anyone who wants to read code written by a fuck
             bnd.Write(oldPath + "\\" + newPath + ".anibnd.dcx", DCX.Type.DCX_KRAK);
             Console.WriteLine("File has been written over at " + oldPath + "\\" + newPath + ".anibnd.dcx");
         }
-        static void importParams(string oldPath) {
-            Console.WriteLine("Please give me the file directory for your list of anibnds you wish to import. \n C:\\Users\\Francis Wang\\Downloads\\Yabber+\\Yabber+\\importList.txt \nYour import list should have cXXXX on each line, so something like c4310 on the first line and c4290 on the second.");
-            string[] lines = System.IO.File.ReadAllLines(Console.ReadLine());
+
+        static void importParamsSeed(string oldPath, string newPath)
+        {
+            string path = oldPath + "\\SwordMasSeed.csv";
+            List<string> lines = System.IO.File.ReadAllLines(path).ToList();
+            List<string> header = lines[0].Split(',').ToList();
+            int paramLength = 0;
+            BND4 bnd = BND4.Read(oldPath + "\\" + newPath + ".anibnd.dcx");
+            Console.WriteLine("Directory being edited is currently " + oldPath + "\\" + newPath + ".anibnd.dcx");
+            IEnumerable<BinderFile> taeFiles = bnd.Files.Where(x => x.Name.Contains(".tae"));
+            foreach (string column in header.Skip(4))
+            {
+                if (column == "s32")
+                {
+                    paramLength += 4;
+                }
+                if (column == "s16")
+                {
+                    paramLength += 2;
+                }
+                if (column == "f32")
+                {
+                    paramLength += 4;
+                }
+                if (column == "b")
+                {
+                    paramLength += 1;
+                }
+                if (column == "u8")
+                {
+                    paramLength += 1;
+                }
+            }
             foreach (string line in lines)
             {
-                importParamsMany(oldPath, line);
+                if (line[0] == 'c')
+                {
+                    newPath = line.Replace(",", "").Substring(0, 5);
+                    bnd.Write(oldPath + "\\" + newPath + ".anibnd.dcx", DCX.Type.DCX_KRAK);
+                    Console.WriteLine("File has been written over at " + oldPath + "\\" + newPath + ".anibnd.dcx");
+
+                    newPath = line.Replace(",", "").Substring(5, 5);
+                    if (newPath == "cXXXX")
+                    {
+                        System.Threading.Thread.Sleep(10000);
+                        System.Environment.Exit(1);
+                    }
+                    bnd = BND4.Read(oldPath + "\\" + newPath + ".anibnd.dcx");
+                    Console.WriteLine("Directory being edited is currently " + oldPath + "\\" + newPath + ".anibnd.dcx");
+                    taeFiles = bnd.Files.Where(x => x.Name.Contains(".tae"));
+
+                    
+                }
+                foreach (var taeFile in taeFiles.Where(x => x.Bytes.Length > 0))
+                {
+                    TAE tae = TAE.Read(taeFile.Bytes);
+                    for (int i1 = 0; i1 < tae.Animations.Count; i1++)
+                    {
+                        TAE.Animation anim = tae.Animations[i1];
+                        if (anim.ID.ToString() == line.Split(',')[0])
+                        {
+                            string[] paramLines = line.Split(',').ToArray();
+                            byte[] rv = new byte[paramLength];
+                            int offset = 0;
+                            for (int i = 4; i < header.Count(); i++)
+                            {
+                                if (header[i] == "s32")
+                                {
+                                    System.Buffer.BlockCopy(BitConverter.GetBytes(Int32.Parse(paramLines[i])), 0, rv, offset, 4);
+                                    offset += 4;
+                                }
+                                if (header[i] == "s16")
+                                {
+                                    System.Buffer.BlockCopy(BitConverter.GetBytes(Int16.Parse(paramLines[i])), 0, rv, offset, 2);
+                                    offset += 2;
+                                }
+                                if (header[i] == "f32")
+                                {
+                                    System.Buffer.BlockCopy(BitConverter.GetBytes(Single.Parse(paramLines[i])), 0, rv, offset, 4);
+                                    offset += 4;
+                                }
+                                if (header[i] == "b")
+                                {
+                                    System.Buffer.BlockCopy(BitConverter.GetBytes(Boolean.Parse(paramLines[i])), 0, rv, offset, 1);
+                                    offset += 1;
+                                }
+                                if (header[i] == "u8")
+                                {
+                                    System.Buffer.BlockCopy(BitConverter.GetBytes(Byte.Parse(paramLines[i])), 0, rv, offset, 1);
+                                    offset += 1;
+                                }
+                            }
+                            TAE.Event addedEvent = new TAE.Event(float.Parse(line.Split(',')[2]), float.Parse(line.Split(',')[3]), int.Parse(line.Split(',')[1]), 0, rv, tae.BigEndian);
+                            addedEvent.Group = new TAE.EventGroup();
+                            addedEvent.Group.GroupType = long.Parse(line.Split(',')[1]);
+                            anim.Events.Add(addedEvent);
+                            Console.WriteLine("New event type " + (float.Parse(line.Split(',')[1]).ToString() + " added at " + float.Parse(line.Split(',')[2]) + " seconds and ending at " + float.Parse(line.Split(',')[3]) + " seconds in in animation " + anim.ID.ToString()));
+                            break;
+                        }
+                    }
+                    taeFile.Bytes = tae.Write();
+                    Console.WriteLine("TAE has been written over.");
+                }
             }
+            
+        }
+
+        static void importParams(string oldPath) {
+            if (Globals.isSeed)
+            {
+                importParamsSeed(oldPath, "c2010");
+            }
+            Console.WriteLine("Please give me the file directory for your list of anibnds you wish to import. \n C:\\Users\\Francis Wang\\Downloads\\Yabber+\\Yabber+\\importList.txt \nYour import list should have cXXXX on each line, so something like c4310 on the first line and c4290 on the second.");
+            string[] lines = System.IO.File.ReadAllLines(Console.ReadLine());
+            if (!Globals.isSeed)
+            {
+                foreach (string line in lines)
+                {
+                    importParamsMany(oldPath, line);
+                }
+            }
+            
+            
             System.Threading.Thread.Sleep(10000);
             System.Environment.Exit(1);
         }
@@ -215,12 +341,18 @@ namespace ConsoleApp4 //Sorry to anyone who wants to read code written by a fuck
             {
                 extractParams(oldPath);
             }
-            Console.WriteLine("Do you wish to access the import function? (y/n)");
-            if (Console.ReadLine() == "y")
+            Console.WriteLine("Do you wish to access the import function? (y/n/s)");
+            string answer = Console.ReadLine();
+            if (answer == "y")
             {
                 importParams(oldPath);
+            } else if (answer == "s")
+            {
+                Globals.isSeed = true;
+                importParams(oldPath);
             }
-            System.Threading.Thread.Sleep(10000);
+
+                System.Threading.Thread.Sleep(10000);
             System.Environment.Exit(1);
         }
         static void Main(string[] args)
